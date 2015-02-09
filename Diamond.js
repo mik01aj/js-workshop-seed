a751852a800d1bc14fe437b9f665251de8bfe146
Mikolaj Dadela <mikolaj.dadela@hgv-online.de>
2015-02-09 13:45:54 +0100

    Added solution.

--- Diamond.js
+++ Diamond.js
@@ -1,15 +1,29 @@
 'use strict';
 
 var Diamond = function (input)  {
-	// TODO do something
+	this.input = input;
 };
 
 Diamond.prototype.getString = function () {
-    return null;
+	var input = this.input;
+	var output = [];
+	for (var i = 0; i < 2 * (input.length) - 1; i++)  {
+        var ii = i >= input.length ? input.length * 2 - 2 - i : i;
+		var line = '';
+		for (var j = 0; j < 2 * (input.length) - 1; j++)  {
+            var jj = j >= input.length ? input.length * 2 - 2 - j : j;
+			line += (ii + jj === input.length - 1) ? input[ii] : '-';
+		}
+		output.push(line);
+	}
+	return output;
 };
 
 Diamond.prototype.print = function () {
-	console.log("Hello world!");
+	var output = this.get();
+	for (var i = 0; i < output.length; i++)  {
+		console.log(output[i]);
+	}
 };
 
 module.exports = Diamond;
